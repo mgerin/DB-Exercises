e5449853e8786f96f7739a03e4cdca0ed2827d2c
mgerin <marin_gerin@abv.bg>
2017-05-09 17:26:05 +0300

    IntroEntity

---
+++ IntroEntity/IntroEntity/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntroEntity.Models;
+
+namespace IntroEntity
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var context = new SoftuniContext();
+            StringBuilder content = new StringBuilder();
+
+            using (context)
+            {
+                // 3.	Employees Full Information
+
+                IEnumerable<Employee> employees = context.Employees;
+
+                foreach (Employee employee in employees)
+                {
+                    content.AppendLine(
+                        $"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary}");
+                }
+
+                File.WriteAllText("../../Output.txt", content.ToString());
+            }
+        }
+    }
+}
